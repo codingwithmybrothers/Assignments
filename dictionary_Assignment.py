8258b594ab3b4722b9e5f8a39dd1cdc9441c59ef
dicksonyankey <yankeydickson1@gmail.com>
2023-05-04 13:02:15 +0000

    Assignments has been posted

---
+++ dictionary_Assignment.py
@@ -0,0 +1,125 @@
+# This a dictionary about a company
+company = {
+    'departments': {
+        'HR': {
+            'employees': {
+                'employee_1': {
+                    'name': 'John Doe',
+                    'position': 'HR Manager',
+                    'salary': 60000
+                },
+                'employee_2': {
+                    'name': 'Jane Smith',
+                    'position': 'HR Assistant',
+                    'salary': 45000
+                }
+            },
+            'policies': ['recruitment', 'onboarding', 'benefits']
+        },
+        'IT': {
+            'employees': {
+                'employee_3': {
+                    'name': 'Alice Brown',
+                    'position': 'IT Manager',
+                    'salary': 70000
+                },
+                'employee_4': {
+                    'name': 'Bob Johnson',
+                    'position': 'Software Developer',
+                    'salary': 65000
+                }
+            },
+            'projects': ['website', 'intranet', 'software development']
+        },
+        'Finance': {
+            'employees': {
+                'employee_5': {
+                    'name': 'Charlie White',
+                    'position': 'Finance Manager',
+                    'salary': 68000
+                },
+                'employee_6': {
+                    'name': 'Eva Green',
+                    'position': 'Accountant',
+                    'salary': 55000
+                }
+            },
+            'budgets': ['operations', 'marketing', 'research']
+        }
+    },
+    'address': {
+        'street': '123 Main St',
+        'city': 'Anytown',
+        'state': 'CA',
+        'zipcode': '12345'
+    },
+    'founders': ['Emily Williams', 'Oliver Taylor'],
+    'year_founded': 2000
+}
+
+
+# Question 1
+# What is the name of the employee with the key employee_3 in the IT department?
+# Hint: You can access the value using dictionary keys in the following format: dictionary[key1][key2][key3].
+# format your output in a nice friendly way
+
+
+# Questions 2
+# What are the policies of the HR department?
+# Hint: Access the value using the appropriate keys in the dictionary.
+# format your output in a nice friendly way
+
+
+# Questions 3
+# What is the total number of employees in the company?
+# Hint: Iterate through the employees in each department and count them.
+# format your output in a nice friendly way
+
+
+# Questions 4
+# Calculate the average salary of all employees in the company.
+# Hint: Iterate through the employees, sum their salaries, and divide by the total number of employees.
+# format your output in a nice friendly way
+
+
+# Questions 5
+# List all the projects the IT department is working on.
+# Hint: Access the value using the appropriate keys in the dictionary.
+# format your output in a nice friendly way
+
+
+# Questions 6
+# Who are the founders of the company?
+# Hint: Access the value using the appropriate key in the dictionary.
+# format your output in a nice friendly way
+
+
+# Questions 7
+# What is the address of the company?
+# Hint: Access the street, city, state, and zipcode values using the
+# appropriate keys in the dictionary and concatenate them.
+# format your output in a nice friendly way
+
+
+# Questions 8
+# Who is the highest paid employee in the company, and what is their salary?
+# Hint: Iterate through the employees in each department, compare their salaries, and
+# keep track of the highest salary and the corresponding employee's name.
+# format your output in a nice friendly way
+
+
+# Questions 9
+# Which department has the most employees?
+# Hint: Iterate through the departments, count the number of employees in each department,
+# and compare the counts to find the department with the most employees.
+# format your output in a nice friendly way
+
+
+# Questions 10
+# List all the employees in the Finance department, along with their positions.
+# Hint: Access the employees dictionary in the Finance department and iterate through it to
+# print the employee names and positions.
+# format your output in a nice friendly way
+
+
+
---
+++ list_Assignment2.py
@@ -0,0 +1,107 @@
+
+# This is a list about a BookShop
+bookshop = [
+    [
+        "Fiction",
+        [
+            ["Mystery", [
+                ["The Hound of the Baskervilles", "Arthur Conan Doyle", 9.99],
+                ["The Maltese Falcon", "Dashiell Hammett", 12.99]
+            ]],
+            ["Science Fiction", [
+                ["Dune", "Frank Herbert", 15.99],
+                ["Neuromancer", "William Gibson", 13.99]
+            ]],
+            ["Fantasy", [
+                ["The Hobbit", "J.R.R. Tolkien", 14.99],
+                ["A Game of Thrones", "George R.R. Martin", 18.99]
+            ]]
+        ]
+    ],
+    [
+        "Non-fiction",
+        [
+            ["Biography", [
+                ["Steve Jobs", "Walter Isaacson", 16.99],
+                ["The Autobiography of Benjamin Franklin", "Benjamin Franklin", 9.99]
+            ]],
+            ["History", [
+                ["Guns, Germs, and Steel", "Jared Diamond", 12.99],
+                ["A People's History of the United States", "Howard Zinn", 19.99]
+            ]],
+            ["Science", [
+                ["A Brief History of Time", "Stephen Hawking", 11.99],
+                ["The Selfish Gene", "Richard Dawkins", 14.99]
+            ]]
+        ]
+    ]
+]
+
+# Question 1
+# What are the two major categories of books in the bookshop?
+# Hint: Access the first element of each of the two outer lists.
+# format your output in a nice friendly way
+
+
+# Question 2
+# How many genres are there under the Fiction category?
+# Hint: Access the list of genres within the Fiction category and use the len() function.
+# format your output in a nice friendly way
+
+
+# Question 3
+# What is the title and author of the second book in the Mystery genre?
+# Hint: Access the appropriate nested lists using indices.
+# format your output in a nice friendly way
+
+
+# Question 4
+# How many genres are there under the Non-fiction category?
+# Hint: Access the list of genres within the Non-fiction category and use the len() function.
+# format your output in a nice friendly way
+
+
+# Question 5
+# What is the title and price of the first book in the Science genre?
+# Hint: Access the appropriate nested lists using indices.
+# format your output in a nice friendly way
+
+
+# Question 6
+# List all the book titles in the Fantasy genre.
+# Hint: Iterate through the books list within the Fantasy genre and print the title of each book.
+# format your output in a nice friendly way
+
+
+# Question 7
+# Calculate the total price of all books in the Biography genre.
+# Hint: Iterate through the books list within the Biography genre, sum their prices,
+# and store the result in a variable.
+# format your output in a nice friendly way
+
+
+# Question 8
+# Find the most expensive book in the Science Fiction genre and provide its title and author.
+# Hint: Iterate through the books list within the Science Fiction genre, compare their prices,
+# and keep track of the highest price and the corresponding book's title and author.
+# format your output in a nice friendly way
+
+
+# Question 9
+# List all the authors of the books in the History genre.
+# Hint: Iterate through the books list within the History genre and print the author of each book.
+# format your output in a nice friendly way
+
+
+# Question 10
+# Calculate the average price of all books in the bookshop.
+# Hint: Iterate through the categories, genres, and books, sum their prices, count
+# the total number of books, and then divide the total price by the total number of books.
+# format your output in a nice friendly way
+
+
+
+
+
+
+

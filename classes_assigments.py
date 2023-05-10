87b264676bfe9d3d9003524925823481eba18c51
dicksonyankey <yankeydickson1@gmail.com>
2023-05-10 19:33:48 +0000

    New Assignment
    This is your new asignment for the week, it will help you practice more on python classes.

---
+++ classes_assigments.py
@@ -0,0 +1,37 @@
+# STUDENT MANAGEMENT SYSTEM
+
+# Question 1 Write a program to build a simple Student Management System using Python which can perform the following
+# operations: 1. Accept : This method takes details from the user like name, roll number, and marks for two different
+# subjects. 2. Display : This method displays the details of every student. 3. Search : This method searches for a
+# particular student from the list of students. This method will ask the user for roll number and then search
+# according to the roll number 4. Delete : This method deletes the record of a particular student with a matching
+# roll number. 5. Update : This method updates the roll number of the student. This method will ask for the old roll
+# number and new roll number. It will replace the old roll number with a new roll number.
+
+
+# Question 2
+# Write a Python class named Rectangle constructed from length and width and a method that will compute
+# the area of a rectangle.
+
+
+# Question 3
+# Write a Python class named Circle constructed from a radius and two methods that will compute the area
+# and the perimeter of a circle.
+
+
+# Question 4
+# Write a Python class Restaurant with attributes like menu_items, book_table, and customer_orders, and methods like
+# add_item_to_menu, book_tables, and customer_order. Perform the following tasks now: Now add items to the menu. Make
+# table reservations. Take customer orders. Print the menu. Print table reservations. Print customer orders. Note:
+# Use dictionaries and lists to store the data.
+
+
+# Question 5
+# Write a Python class BankAccount with attributes like account_number, balance, date_of_opening and customer_name,
+# and methods like deposit, withdraw, and check_balance.
+
+
+# Question 6
+# Write a Python class Inventory with attributes like item_id, item_name, stock_count, and price,
+# and methods like add_item, update_item, and check_item_details. Use a dictionary to store the item details,
+# where the key is the item_id and the value is a dictionary containing the item_name, stock_count, and price.
